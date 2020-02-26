5863ccbf0bc713f922ec948a29d23d915c060373
creatina98 <34490820+creatina98@users.noreply.github.com>
2020-02-26 11:53:09 +0200

    take that s*it

--- Poze.js
+++ Poze.js
@@ -3,15 +3,16 @@ $('.text1').hide();
 $('.text2').hide();
 
 $('.column1').on('mouseenter', () => {
-    $('#e').hide();
-    $('.text1').show();
+    $('#e').hide(200);
+    $('.text1').show('slow');
+    
 }).on('mouseleave', () => {
     $('#e').show();
     $('.text1').hide();
 });
-$('#D').on('mouseenter', () => {
-    $('#m').hide();
-    $('.text2').show();
+$('.column2').on('mouseenter', () => {
+    $('#m').hide(500);
+    $('.text2').show(500);
 }).on('mouseleave', () => {
     $('#m').show();
     $('.text2').hide();
