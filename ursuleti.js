b6b0a091bfd0c00d2baf9c7a4927be2172752b10
PreotulKazoo <60981909+PreotulKazoo@users.noreply.github.com>
2020-03-11 22:39:13 +0200

    ytyyy

--- ursuleti.js
+++ ursuleti.js
@@ -6,11 +6,14 @@ window.addEventListener('scroll',() => {
     if (window.scrollY>=650 && window.scrollY<=1300)
     {
       $("#ursulet1").animate({
-        top: "-20"},1000);
+        top: "-20"
+      },1000);
       $("#labutaS1").animate({
-        top: "+80"},1000);
-        $("#labutaD1").animate({
-            top: "+80"},1000);
+        top: "+80"
+      },1000);
+      $("#labutaD1").animate({
+            top: "+80"
+      },1000);
     };
     if (window.scrollY>=1550 && window.scrollY<=2000)
     {
